62067267c087de8bba06b8dd940c6fc12a5e93af
edaywalid <becharwalid9@gmail.com>
2023-12-01 10:26:45 +0100

    feat : added AppliCompteBancaire class

---
+++ poo/tp3/exercice1/AppliCompteBancaire.java
@@ -0,0 +1,11 @@
+package tp3.exercice1;
+
+
+public class AppliCompteBancaire {
+    public static void main(String[] args) {
+        CompteBancaire compteBancaire = new CompteBancaire();
+        // tester la methode toString
+        System.out.println(compteBancaire.toString());
+        
+    }
+}
